cbf9809907dce86a415738ba2e6c75a666d3c2ff
saadhya2010 <saadhya2010@gmail.com>
2026-04-02 14:50:24 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ This will install the packages from requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = 'key_here'
 Bootstrap5(app)
 
 
